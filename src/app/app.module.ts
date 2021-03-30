35cb328a6c6cbe39ee8f66e64ba05fe10e4acddf
Walter Ishibashi <wishibashi@gmail.com>
2021-03-30 14:53:35 -0300

    Adding the AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,22 @@
+// Angular imports
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router'
 
+// component imports
 // As boas práticas recomendam colocar as importações em ordem alfabética
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+
+// services imports
 import { TaskService } from './tasks/shared/task.service';
 
-const ROUTES = RouterModule.forRoot([   // forRoot gera um módulo com todas os providers e diretivas de rotas 
-  {
-    path: 'tasks/:id',
-    component: TaskDetailComponent
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
-  {
-    path: 'tasks',
-    component: TasksComponent
-  },
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
-  }
-])
+// routing imports
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -44,7 +30,7 @@ const ROUTES = RouterModule.forRoot([   // forRoot gera um módulo com todas os 
     BrowserModule,
     FormsModule,
     HttpModule,
-    ROUTES        // contém todos os módulos necessários, por isso não é necessário importar RouterModule
+    AppRoutingModule
   ],
  // providers: [
     //   { provide: TaskService, useClass: TaskService } // quando alguém perguntar por TaskService (provide),
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+
+const ROUTES = RouterModule.forRoot([   // forRoot gera um módulo com todas os providers e diretivas de rotas 
+    { path: 'tasks/:id', component: TaskDetailComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'tasks', component: TasksComponent },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+  ])
+
+  @NgModule({
+    imports: [ROUTES],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
